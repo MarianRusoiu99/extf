3f0a88a1b5de247aa429b2abc71838c99480a464
ValentinRusoiu99 <valentinrusoiu@gmail.com>
2023-09-19 14:12:45 +0300

    making reusable our process component

--- components/OurProcess/index.jsx
+++ components/OurProcess/index.jsx
@@ -3,168 +3,80 @@ import React from 'react'
 
 
 function OurProcess() {
+
+  const content = [{
+    title: "Penta Consulting",
+    subtitleBanner: { first: "Front End Developer", second: "June 2016 - Present" },
+    content: "Working alongside the designer team implementing the designs, also developing custom solutions to address team necessities.",
+    tags: ["HTML5", "HTML5", "HTML5"]
+
+  },
+  {
+    title: "Penta Consulting",
+    subtitleBanner: { first: "Front End Developer", second: "June 2016 - Present" },
+    content: "Working alongside the designer team implementing the designs, also developing custom solutions to address team necessities.",
+    tags: ["HTML5", "HTML5", "HTML5", "HTML5"]
+
+  },
+  {
+    title: "Penta Consulting",
+    subtitleBanner: { first: "Front End Developer", second: "June 2016 - Present" },
+    content: "Working alongside the designer team implementing the designs, also developing custom solutions to address team necessities.",
+    tags: ["HTML5", "HTML5", "HTML5", "HTML5"]
+
+  },
+  {
+    title: "Penta Consulting",
+    subtitleBanner: { first: "Front End Developer", second: "June 2016 - Present" },
+    content: "Working alongside the designer team implementing the designs, also developing custom solutions to address team necessities.",
+    tags: ["HTML5", "HTML5", "HTML5", "HTML5"]
+
+  },{
+    title: "Penta Consulting",
+    subtitleBanner: { first: "Front End Developer", second: "June 2016 - Present" },
+    content: "Working alongside the designer team implementing the designs, also developing custom solutions to address team necessities.",
+    tags: ["HTML5", "HTML5", "HTML5", "HTML5"]
+
+  }
+  ]
+
+
+
   return (
     <>
-    
-    
-    <section id="cd-timeline" class="cd-container">
-		<div class="cd-timeline-block">
-			<div class="cd-timeline-img cd-picture">
-			</div>
+      <section id="cd-timeline" class="cd-container">
 
-			<div class="cd-timeline-content">
-				<h2>Penta Consulting</h2>
-        <div class="timeline-content-info">
-          <span class="timeline-content-info-title">
-            <i class="fa fa-certificate" aria-hidden="true"></i>
-            Front End Developer
-          </span>
-          <span class="timeline-content-info-date">
-            <i class="fa fa-calendar-o" aria-hidden="true"></i>
-            June 2016 - Present
-          </span>
-        </div>
-				<p>Working alongside the designer team implementing the designs, also developing custom solutions to address team necessities.</p>
-        <ul class="content-skills">
-        <li>HTML5</li>
-        <li>CSS3</li>
-        <li>JavaScript</li>
-        <li>jQuery</li>
-        <li>Wordpress</li>
-        </ul>
-			</div>
-		</div> 
-        <div class="cd-timeline-block">
-			<div class="cd-timeline-img cd-picture">
-			</div>
-
-			<div class="cd-timeline-content">
-				<h2>Penta Consulting</h2>
-        <div class="timeline-content-info">
-          <span class="timeline-content-info-title">
-            <i class="fa fa-certificate" aria-hidden="true"></i>
-            Front End Developer
-          </span>
-          <span class="timeline-content-info-date">
-            <i class="fa fa-calendar-o" aria-hidden="true"></i>
-            June 2016 - Present
-          </span>
-        </div>
-				<p>Working alongside the designer team implementing the designs, also developing custom solutions to address team necessities.</p>
-        <ul class="content-skills">
-        <li>HTML5</li>
-        <li>CSS3</li>
-        <li>JavaScript</li>
-        <li>jQuery</li>
-        <li>Wordpress</li>
-        </ul>
-			</div>
-		</div> 
-        <div class="cd-timeline-block">
-			<div class="cd-timeline-img cd-picture">
-			</div>
-
-			<div class="cd-timeline-content">
-				<h2>Penta Consulting</h2>
-        <div class="timeline-content-info">
-          <span class="timeline-content-info-title">
-            <i class="fa fa-certificate" aria-hidden="true"></i>
-            Front End Developer
-          </span>
-          <span class="timeline-content-info-date">
-            <i class="fa fa-calendar-o" aria-hidden="true"></i>
-            June 2016 - Present
-          </span>
-        </div>
-				<p>Working alongside the designer team implementing the designs, also developing custom solutions to address team necessities.</p>
-        <ul class="content-skills">
-        <li>HTML5</li>
-        <li>CSS3</li>
-        <li>JavaScript</li>
-        <li>jQuery</li>
-        <li>Wordpress</li>
-        </ul>
-			</div>
-		</div> 
-        <div class="cd-timeline-block">
-			<div class="cd-timeline-img cd-picture">
-			</div>
-
-			<div class="cd-timeline-content">
-				<h2>Penta Consulting</h2>
-        <div class="timeline-content-info">
-          <span class="timeline-content-info-title">
-            <i class="fa fa-certificate" aria-hidden="true"></i>
-            Front End Developer
-          </span>
-          <span class="timeline-content-info-date">
-            <i class="fa fa-calendar-o" aria-hidden="true"></i>
-            June 2016 - Present
-          </span>
-        </div>
-				<p>Working alongside the designer team implementing the designs, also developing custom solutions to address team necessities.</p>
-        <ul class="content-skills">
-        <li>HTML5</li>
-        <li>CSS3</li>
-        <li>JavaScript</li>
-        <li>jQuery</li>
-        <li>Wordpress</li>
-        </ul>
-			</div>
-		</div> 
-        <div class="cd-timeline-block">
-			<div class="cd-timeline-img cd-picture">
-			</div>
-
-			<div class="cd-timeline-content">
-				<h2>Penta Consulting</h2>
-        <div class="timeline-content-info">
-          <span class="timeline-content-info-title">
-            <i class="fa fa-certificate" aria-hidden="true"></i>
-            Front End Developer
-          </span>
-          <span class="timeline-content-info-date">
-            <i class="fa fa-calendar-o" aria-hidden="true"></i>
-            June 2016 - Present
-          </span>
-        </div>
-				<p>Working alongside the designer team implementing the designs, also developing custom solutions to address team necessities.</p>
-        <ul class="content-skills">
-        <li>HTML5</li>
-        <li>CSS3</li>
-        <li>JavaScript</li>
-        <li>jQuery</li>
-        <li>Wordpress</li>
-        </ul>
-			</div>
-		</div> 
-        <div class="cd-timeline-block">
-			<div class="cd-timeline-img cd-picture">
-			</div>
-
-			<div class="cd-timeline-content">
-				<h2>Penta Consulting</h2>
-        <div class="timeline-content-info">
-          <span class="timeline-content-info-title">
-            <i class="fa fa-certificate" aria-hidden="true"></i>
-            Front End Developer
-          </span>
-          <span class="timeline-content-info-date">
-            <i class="fa fa-calendar-o" aria-hidden="true"></i>
-            June 2016 - Present
-          </span>
-        </div>
-				<p>Working alongside the designer team implementing the designs, also developing custom solutions to address team necessities.</p>
-        <ul class="content-skills">
-        <li>HTML5</li>
-        <li>CSS3</li>
-        <li>JavaScript</li>
-        <li>jQuery</li>
-        <li>Wordpress</li>
-        </ul>
-			</div>
-		</div> 
-        	</section>     </>
+        {content.map((content,index)=>{
+          return(
+            <div class="cd-timeline-block" key={index}>
+            <div class="cd-timeline-img cd-picture">
+            </div>
+  
+            <div class="cd-timeline-content">
+              <h2>{content.title}</h2>
+              <div class="timeline-content-info">
+                <span class="timeline-content-info-title">
+                  <i class="fa fa-certificate" aria-hidden="true"></i>
+                  {content.subtitleBanner.first}
+                </span>
+                <span class="timeline-content-info-date">
+                  <i class="fa fa-calendar-o" aria-hidden="true"></i>
+                  {content.subtitleBanner.second}
+                </span>
+              </div>
+              <p>{content.content}</p>
+              <ul class="content-skills">
+               {content.tags.map((c,i)=>{
+                return(
+                  <li key={i}>{c}</li>
+                )
+               })}
+              </ul>
+            </div>
+          </div>
+          )
+        })}     
+      </section>     </>
   )
 }
 
